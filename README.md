a96e476b46a00b17fba37238d62a37a7c265cf13
Omig12 <israelodilan@gmail.com>
2018-02-13 09:53:11 -0400

    Fixing analysis.py, adding more descriptive readme

--- README.md
+++ README.md
@@ -2,13 +2,43 @@
 
 In this repository we will attempt to reproduce a research pipeline that employs a heuristic pairwise alignment algorithm to perform differential expression analysis on Sea Cucumber genetic samples, more specifically on regenerative intestinal tissue samples vs. non regenerative ones.
 
-----------------------------------------------------------
+## Files:
++ **dbg_optimized.py:**
+	+ Usage: 
+		```sh  
+		$ python3 dbg_optimized.py -k <k-mer lenght> -A <fastq A> -B <fastq B> -o <outputfilename> 
+		```
+	
+	+ Ouput:
+		+ GFA encoded graph with differential expression information anotated
+		```txt  
+		H	VN:Z:1.0
+		S	0:0:(A:1,B:1)	AAGTTGCGCTAGGGTTAAACT
+		S	0:1:(A:1,B:1)	AGTTGCGCTAGGGTTAAACTC
+		L	0:0:(A:1,B:1)	+	0:1:(A:1,B:1)	+	20M
+		```
+
++ **analysis.py:**
+	+ Usage: 
+		```sh 
+		$ python3 analysis.py -f <filename> [-t <threshold>] [-o <outputfilename>]
+		```
+
+	+ Ouput:
+		+ GFA encoded link with differential expression more than or equal to threshold, plus differential expression coeffient.
+		```txt  
+		L       0:0:(A:1,B:1)   +       0:1:(A:1,B:1)   +       20M
+		Differential Coefficient: 0.0
+		```
+
 ## Worked continued on the [DBGDE](https://github.com/Omig12/DBGDE) repository.
 
+----------------------------------------------------------
+
 ## Proposed (DONE) Workflow:
- + Build De-bruijn Graph
-   - Might use [this](https://pmelsted.wordpress.com/2013/11/23/naive-python-implementation-of-a-de-bruijn-graph/) approach.
- + Count the proportion/distribution of kmers recovered from each organism.
+ + Build De-bruijn Graph:
+   - Used [this](https://pmelsted.wordpress.com/2013/11/23/naive-python-implementation-of-a-de-bruijn-graph/) approach.
+ + Counted the proportion/distribution of kmers recovered from each organism.
  + Perform differential analysis on that data
 
 ## Collaborators:
--- analysis.py
+++ analysis.py
@@ -8,7 +8,7 @@
 # organisms.                                                                             #
 #                                                                                        #
 # To run the program, utilize the command:                                               #
-# $ "Python3 Second_Program.py GFA -f <filename>"                               #
+# $ "python3 analysis.py -f <filename> [-t <threshold>] [-o <outputfilename>]"           #
 # The GFA file utilized must be located on the same directory as the parser or full path #
 # must be specified.                                                                     #
 #                                                                                        #
@@ -18,50 +18,58 @@
 # Load Modules # 
 ################
 
-import re
-import os
-import sys
 import argparse
+import re
+from sys import exit
+# import os
 
-def main():
-    regex = r"^L\t(\d+:\d+):\(A:(\d+),B:(\d+)\)\t[+-]\t(\d+:\d+):\(A:(\d+),B:(\d+)\)"
-    prog = re.compile(regex, flags = re.M)
-    with open(args.file,'r') as file:
-        for line in file.readlines():
-            if line[0] != "L":
-                continue
-            else:
-                # print(line)
-                g = prog.match(line)
-                A = [int(g.group(2)),int(g.group(5))]
-                B = [int(g.group(3)),int(g.group(6))]
-                coef = DiffExp(A,B)
-                if  coef <= args.threshold:
-                    with open(args.output ,'a+') as out:
-                        out.write(line + "Differential Coefficient: " + str(coef)+ "\n\n" )           
-                else:
-                    continue
-                    # if os.path.isfile(args.output):
-                    #     print(line[:-1])
-                    #     # print({g.group(1):{"A":g.group(2), "B": g.group(3)},g.group(4):{"A":g.group(5), "B": g.group(6)} })
-                    #     print(coef)
-                    #     print("Did not write to file")
-                    # else:
-                        # with open(args.output ,'a+') as out:
-                        #     out.write(line + "Differential Coefficient: " + str(coef)+ "\n\n" )           
+# Delta A + Delta B / Over k-mer total
+def DiffExp(kA, kB):
+	'''Calculates differential expression between two node on the gfa graph that are linked.'''
+	return ( (abs(kA[0]-kA[1]) + abs(kB[0]-kB[1])) / (kA[0] + kA[1] + kB[0] + kB[1]) )
 
 # A Delta B / Over k-mer total 
-def DiffExp(kA, kB):
-    return (abs((kA[0] + kA[1]) - (kB[0] + kB[1]))/(kA[0] + kA[1] + kB[0] + kB[1]))
+# def DiffExpresion2(kA, kB):
+#     return (abs((kA[0] + kA[1]) - (kB[0] + kB[1]))/(kA[0] + kA[1] + kB[0] + kB[1]))
 
+def main():
+	'''Main Program function runs parser, differential expression and output'''
+	regex = r"^L\t(\d+:\d+):\(A:(\d+),B:(\d+)\)\t[+-]\t(\d+:\d+):\(A:(\d+),B:(\d+)\)"
+	prog = re.compile(regex, flags=re.M)
+	with open(args.file,'r') as file:
+		for line in file.readlines():
+			if line[0] != "L":
+				continue
+			else:
+				g = prog.match(line)
+				A = [int(g.group(2)),int(g.group(5))]
+				B = [int(g.group(3)),int(g.group(6))]
+				coef = DiffExp(A,B)
+				# print(line + coef"Differential Coefficient: " + str(coef)+ "\n\n")
+				if  coef >= args.threshold:
+					with open(args.output,"a+") as out:
+						out.write(line + "Differential Coefficient: " + str(coef)+ "\n\n" )           
+				else:
+					continue
+	# if os.path.isfile(args.output):
+	#     # print({g.group(1):{"A":g.group(2), "B": g.group(3)},g.group(4):{"A":g.group(5), "B": g.group(6)} })
+	#     print("Did not write to file")
+	# else:
+	# with open(args.output ,'a+') as out:
+	#     out.write(line + "Differential Coefficient: " + str(coef)+ "\n\n" )           
 
 parser = argparse.ArgumentParser(description="Differential Expression Coefficient")
 parser.add_argument('-f', '--file', required=True, help="The GFA input file with Differential Expression info, ouput from dbg.py")
-parser.add_argument('-t','--threshold', default = 1.0, type= float, required=False, help="The threshold of differential expression, only show links with similarity less than float value given. (default: 1.0 'Identical')")
+parser.add_argument('-t','--threshold', default = 0.0, type= float, required=False, help="The threshold of differential expression, only show links with more differentially expressed than the float value t given, where 0 =< t <= 1. (default: 0.0 'Identical')")
 parser.add_argument('-o','--output', default="diff_exp.txt",required=False,help="Output GFA file name. (default: 'diff_exp.txt')")
-
 args = parser.parse_args()
 
-
 if __name__ == "__main__":
-    main()
+	if args.file[-3:] != "gfa":
+		if input("File does not appear to be a GFA, Do you wish to continue (y/n)? ") == "y":
+			main()
+		else:
+			exit(0)
+	else:
+		main()
+	
